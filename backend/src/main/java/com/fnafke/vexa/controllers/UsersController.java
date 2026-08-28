a14975c3fabcfd8baf4d73ceefeb627391a96ee6
Fnafke <elinbachvarov@gmail.com>
2026-08-28 22:22:35 +0200

    Added a /me endpoint

--- backend/src/main/java/com/fnafke/vexa/controllers/UsersController.java
+++ backend/src/main/java/com/fnafke/vexa/controllers/UsersController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,13 @@ public class UsersController {
     }
 
     // GET MAPPINGS
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> getCurrentUser(Authentication authentication) {
+        String username = authentication.getName();
+        UserDto userDto = new UserDto(userService.findByUsername(username));
+        return ResponseEntity.ok(userDto);
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserDto> getUserById(@PathVariable UUID id) {
