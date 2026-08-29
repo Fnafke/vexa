851ef3dd4e7b06045611550ba789fa2b5d17e71c
Fnafke <elinbachvarov@gmail.com>
2026-08-29 13:04:52 +0200

    Changed Authentication type to @AuthenticationPrincipal so I get the current user for everything.

--- backend/src/main/java/com/fnafke/vexa/controllers/UsersController.java
+++ backend/src/main/java/com/fnafke/vexa/controllers/UsersController.java
@@ -5,12 +5,14 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fnafke.vexa.controllers.dto.UserDto;
+import com.fnafke.vexa.models.User;
 import com.fnafke.vexa.services.interfaces.UserService;
 
 @RestController
@@ -28,9 +30,8 @@ public class UsersController {
 
     // /api/users/me - Get the currently authenticated user's information
     @GetMapping("/me")
-    public ResponseEntity<UserDto> getCurrentUser(Authentication authentication) {
-        String username = authentication.getName();
-        UserDto userDto = new UserDto(userService.findByUsername(username));
+    public ResponseEntity<UserDto> getCurrentUser(@AuthenticationPrincipal User currentUser) {
+        UserDto userDto = new UserDto(currentUser);
         return ResponseEntity.ok(userDto);
     }
 
